71fa2bb3271da6e55d7cd28c17bd2c81609221ee
Rajesh01-star <rajeshbehera489@gmail.com>
2024-12-25 01:22:12 +0530

    new touch

--- app/components/ImageGrid.jsx
+++ app/components/ImageGrid.jsx
@@ -1,16 +1,23 @@
+import { motion } from "framer-motion";
 import images from "../data/images";
 
 export default function ImageGrid() {
   return (
     <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6 relative">
       {images.map((image, index) => (
-        <img
+        <motion.img
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5 }}
           key={index}
           src={image.src}
           alt={image.alt}
           className={`${
             index > 1 ? " hidden lg:block" : "block"
-          } w-full h-auto rounded-lg transform cursor-pointer lg:${image.rotationClass}`}
+          } w-full h-auto rounded-lg transform cursor-pointer lg:${
+            image.rotationClass
+          }`}
         />
       ))}
     </div>
